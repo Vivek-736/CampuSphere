24d90c5ebc8e83485618672b8928cf5501b59dfb
Vivek <vivekousik8555@gmail.com>
2025-08-17 10:30:36 +0530

    Updated the layout and fetched the profile image of the user

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Ionicons from '@expo/vector-icons/Ionicons'
+import { useUser } from '@/context/UserContext'
+import { Image } from 'react-native'
 
 export default function TabLayout() {
+    const { userData } = useUser()
+
     return (
         <Tabs>
             <Tabs.Screen name="Home"
@@ -10,19 +14,37 @@ export default function TabLayout() {
                     tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />
                 }}
             />
+
             <Tabs.Screen name="Event"
                 options={{
                     tabBarIcon: ({ color, size }) => <Ionicons name="calendar" size={size} color={color} />
                 }}
             />
+
             <Tabs.Screen name="Clubs"
                 options={{
                     tabBarIcon: ({ color, size }) => <Ionicons name="people" size={size} color={color} />
                 }}
             />
+            
             <Tabs.Screen name="Profile"
                 options={{
-                    tabBarIcon: ({ color, size }) => <Ionicons name="person-circle" size={size} color={color} />
+                    tabBarIcon: ({ color, size }) => (
+                        userData?.image ? (
+                            <Image 
+                                source={{ uri: userData.image }} 
+                                style={{ 
+                                    width: size, 
+                                    height: size, 
+                                    borderRadius: size / 2,
+                                    borderWidth: 1,
+                                    borderColor: color
+                                }} 
+                            />
+                        ) : (
+                            <Ionicons name="person-circle" size={size} color={color} />
+                        )
+                    )
                 }}
             />
         </Tabs>
